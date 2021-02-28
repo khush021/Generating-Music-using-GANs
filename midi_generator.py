57d90c72ed0243131f24b1b7dad095ddeb222e8a
OroArmor <eliorona@live.com>
2021-02-27 20:19:52 -0800

    Generate midi files from the network

---
+++ midi_generator.py
@@ -0,0 +1,21 @@
+from midi_data_extractor import PlayedNote
+from music21 import *
+
+def convert_data_to_midi(input_data, name):
+    print("Turning data into midi stream")
+    _stream = stream.Stream()
+    for x in range(len(input_data[0])) :
+        notes = []
+        for y in range(len(input_data)):
+            if(input_data[y][x] > 0.5):
+                notes.append(note.Note(96 - y))
+        midi_note = None
+        if len(notes) > 0:
+            midi_note = chord.Chord(notes)
+        else:
+            midi_note = note.Note(0)
+        midi_note.duration.quarterLength = 0.25
+        _stream.append(midi_note)
+    print(f"Creating midi file with name {name}")
+    _converter = converter.Converter().getSubConverterFormats()["midi"]().write(_stream, "midi", f"{name}.mid")
+    return stream
--- network/network.py
+++ network/network.py
@@ -2,6 +2,8 @@ from pygan.ebgan_image_generator import EBGANImageGenerator
 from logging import getLogger, StreamHandler, NullHandler, DEBUG, ERROR
 import mxnet as mx
 from PIL import Image, ImageDraw
+from midi_generator import convert_data_to_midi
+import sys
 
 logger = getLogger("accelbrainbase")
 handler = StreamHandler()
@@ -10,6 +12,7 @@ logger.setLevel(DEBUG)
 logger.addHandler(handler)
 
 def train_network():
+    print("Creating model")
     ebgan_image_generator = EBGANImageGenerator(
         # `list` of path to your directories.
         dir_list=[
@@ -22,40 +25,34 @@ def train_network():
         # `int` of image channel.
         channel=1,
         # `int` of batch size.
-        batch_size=40,
+        batch_size=200,
         # `float` of learning rate.
         learning_rate=1e-06,
         # context for the network to run in
         ctx = mx.cpu()
     )
 
+    print("Loading model from file")
     try:
         ebgan_image_generator.EBGAN.generative_model.load_parameters("networks/edmgan.network")
         pass
     except Exception as e:
-        print("no model found")
+        print("No model found.")
 
+    epochs = 1
+    if len(sys.argv) > 2:
+        epochs = int(sys.argv[2])
+
+    print("Training model")
     ebgan_image_generator.learn(
         # `int` of the number of training iterations.
-        iter_n=1,
+        iter_n=epochs,
         # `int` of the number of learning of the discriminative model.
         k_step=10,
     )
 
+    print("Saving model to file")
     ebgan_image_generator.EBGAN.generative_model.save_parameters("networks/edmgan.network")
-
-    arr = ebgan_image_generator.EBGAN.generative_model.draw().asnumpy()
-    image = arr[0, 0]
-    img = Image.new(size = (128, 96), mode = "RGB")
-    img_drawer = ImageDraw.Draw(img)
-    print(arr)
-    for y in range(len(image)):
-        for x in range(len(image[y])):
-            print(image[y, x])
-            g = int(255 * (1 if image[y][x] > 0.5 else 0))
-            img_drawer.point((x,y), fill = (g,g,g))
-    img.show()
-    print("Training model")
 
 def generate_output():
     ebgan_image_generator = EBGANImageGenerator(
@@ -72,7 +69,7 @@ def generate_output():
         # `int` of batch size.
         batch_size=1,
         # `float` of learning rate.
-        learning_rate=1e-06,
+        learning_rate=0,
         # context for the network to run in
         ctx = mx.cpu()
     )
@@ -80,6 +77,27 @@ def generate_output():
         ebgan_image_generator.EBGAN.generative_model.load_parameters("networks/edmgan.network")
         pass
     except Exception as e:
-        print("no model found")
+        print("No model found")
+        return
     print("Generating output")
-    return arr = ebgan_image_generator.EBGAN.generative_model.draw().asnumpy()[0, 0]
+    arr = ebgan_image_generator.EBGAN.generative_model.draw().asnumpy()
+    image = arr[0, 0]
+    img = Image.new(size = (128, 96), mode = "RGB")
+    img_drawer = ImageDraw.Draw(img)
+    for y in range(len(image)):
+        for x in range(len(image[y])):
+            g = 255
+            if image[y][x] < 0.5:
+                g = 0
+            img_drawer.point((x,y), fill = (g,g,g))
+
+    print("Showing Image")
+    img.show()
+
+    name = "test"
+    if len(sys.argv) > 2:
+        name = sys.argv[2]
+
+    print("Saving to midi file")
+    midi = convert_data_to_midi(image, name)
+    # return {image, midi, img}
